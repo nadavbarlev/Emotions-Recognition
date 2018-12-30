45744813dd64481406c62a21dfa669dbfc7f0bad
Nadav Bar Lev <nadav.bar.lev@gmail.com>
2018-12-30 21:41:49 +0200

    Initial Commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Nadav Bar Lev on 30/12/2018.
+//  Copyright © 2018 Nadav Bar Lev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
