98ab8036dd09c35639151135d418dd0e5bde6b40
Ofir Lauber <OfirLauber@gmail.com>
2019-01-05 19:41:51 +0200

    Add initial server side work

---
+++ Server/controller.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import numpy as np
+import base64
+from emotion_detector import EmotionDetector
+
+app = Flask(__name__)
+
+
+@app.route("/v1/detect", methods=['POST'])
+def detect_emotion():
+    base64_image = request.form['base64_image']
+    image_array = np.fromstring(base64.b64decode(base64_image), np.uint8)
+    return EmotionDetector().detect_emotion(image_array)
